57967b1e2bfb78f686fe919617c88f53e865e209
DroidZed <41507665+DroidZed@users.noreply.github.com>
2023-08-20 15:38:17 +0100

    smth is wrong

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,20 +3,53 @@ package controllers
 // CRUD: user
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 
+	"github.com/DroidZed/go_lance/config"
+	"github.com/DroidZed/go_lance/db"
+	"github.com/DroidZed/go_lance/models"
+	"github.com/DroidZed/go_lance/utils"
 	"github.com/go-chi/chi"
+	"go.mongodb.org/mongo-driver/bson"
+
 )
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("All users here"))
+	coll := db.Client.Database(config.EnvDbName()).Collection("users")
+
+	cursor, err := coll.Find(context.TODO(), nil)
+
+	if err != nil {
+		utils.JsonResponse(w, 400, utils.DtoResponse{Error: "Could not decode data!"})
+	}
+
+	var results []models.User = make([]models.User, 0)
+
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+
+	utils.JsonResponse(w, 200, results)
 
 }
 
 func GetUserById(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	w.Write([]byte(fmt.Sprintf("User with id: %s", id)))
+	coll := db.Client.Database(config.EnvDbName()).Collection("users")
+
+	filter := bson.D{{Key: "_id", Value: id}}
+
+	var result models.User = models.User{}
+
+	err := coll.FindOne(context.TODO(), filter).Decode(result)
+
+	if err != nil {
+		utils.JsonResponse(w, 400, utils.DtoResponse{Error: "Could not decode data!"})
+	}
+
+	utils.JsonResponse(w, 200, result)
+
 }

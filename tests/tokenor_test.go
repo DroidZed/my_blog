aecfdd84d22bdba0c7073fca2ad36beaf566b556
DroidZed <41507665+DroidZed@users.noreply.github.com>
2023-09-06 10:15:20 +0100

    added missing line for loading env in tests

--- tests/tokenor_test.go
+++ tests/tokenor_test.go
@@ -3,11 +3,14 @@ package tests
 import (
 	"testing"
 
+	"github.com/DroidZed/go_lance/internal/config"
 	"github.com/DroidZed/go_lance/internal/cryptor"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGeneratingToken(t *testing.T) {
+
+	config.LoadEnv()
 
 	token, err := cryptor.GenerateAccessToken("5s4dasd")
 
